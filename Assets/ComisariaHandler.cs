3f4cdbe866d091bb816b982450b9a88537dd96b9
mangel119 <miguel.sanabria@uao.edu.co>
2022-05-28 19:35:51 -0500

    Dialogos Comisaria

--- Assets/ComisariaHandler.cs
+++ Assets/ComisariaHandler.cs
@@ -6,27 +6,37 @@ public class ComisariaHandler : MonoBehaviour
 {
     public GameObject bubbleUi;
     private GameObject bubbleUse;
-    private string textLidia1 = "Inspector, ¡ayuda!";
     private Vector3 localPosition = new Vector3 (-0.7f,1.6f);
     [SerializeField] private Transform lidiaTransform;
     [SerializeField] private Transform inspectorTransform;
     [SerializeField] private Transform felixTransform;
+    [SerializeField] private GameObject tutorial;
+    
     // Start is called before the first frame update
     void Start()
     {
-        bubbleUse = Instantiate(bubbleUi, FindObjectOfType<Canvas>().transform);
-        bubble();
-        
+        bubble(lidiaTransform, "Inspector, ¡ayuda!");
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(bubbleUse);
+            Destroy(tutorial);
+            bubble(inspectorTransform, "Es Lidia Acosta, ¿Qué ocurrió?");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Destroy(bubbleUse);
+                bubble(lidiaTransform, "Han entrado a mi casa y me han robado mis joyas");
+            }
+        }
     }
 
-    private void bubble()
+    private void bubble(Transform charTransform, string text)
     {
-        bubbleUse.transform.position = Camera.main.WorldToScreenPoint(lidiaTransform.position+localPosition);
-        bubbleUse.GetComponent<ChatBubble>().Setup(textLidia1);
+        bubbleUse = Instantiate(bubbleUi, FindObjectOfType<Canvas>().transform);
+        bubbleUse.transform.position = Camera.main.WorldToScreenPoint(charTransform.position+localPosition);
+        bubbleUse.GetComponent<ChatBubble>().Setup(text);
     }
 }
