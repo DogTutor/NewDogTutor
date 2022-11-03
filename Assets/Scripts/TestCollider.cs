d0845291bb5fc13b5cf45a9f19b5bdd6c28d7c8a
MiguelSanabria <mangel.sanabria119@gmail.com>
2022-11-03 00:44:43 -0500

    Implementados todos los dialogos e interacciones

--- Assets/Scripts/TestCollider.cs
+++ Assets/Scripts/TestCollider.cs
@@ -11,8 +11,6 @@ public class TestCollider : MonoBehaviour
   private StoryCinematicDirector director;
   // Variables Flag
   private float relojFlag = 0;
-  private float saveFlag = 0;
-  private float chocolateFlag = 0;
   private float brokeFlag = 0;
   private float windowFlag = 0;
   private float ardillaFlag = 0;
@@ -20,7 +18,7 @@ public class TestCollider : MonoBehaviour
 
 
   [SerializeField] private Transform lidiaTransform;
-  [SerializeField] private Transform adelaTransform;
+  [SerializeField] public Transform adelaTransform;
   [SerializeField] private Transform felixTransform;
   [SerializeField] private Transform rolloTransform;
   [SerializeField] private Transform kikiTransform;
@@ -46,23 +44,12 @@ public class TestCollider : MonoBehaviour
       bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
       if (hit)
       {
+        // Pistas
         if (hitInfo.transform.gameObject.name == "InteractableClock" && relojFlag == 0)
         {
           Destroy(bubbleUse);
           bubble(adelaTransform, "Un reloj caído detenido 5 minutos después de media noche");
           relojFlag = 1;
-        }
-        if (hitInfo.transform.gameObject.name == "InteractableSafe" && saveFlag == 0)
-        {
-          Destroy(bubbleUse);
-          bubble(kikiTransform, "La caja fuerte está intacta");
-          saveFlag = 1;
-        }
-        if (hitInfo.transform.gameObject.name == "InteractableChocolate" && chocolateFlag == 0)
-        {
-          Destroy(bubbleUse);
-          bubble(lidiaTransform, "Pueden comer de los bombones de chocolate \n como agradecimiento");
-          chocolateFlag = 1;
         }
         if (hitInfo.transform.gameObject.name == "InteractableNews" && periodicoFlag == 0)
         {
@@ -91,7 +78,31 @@ public class TestCollider : MonoBehaviour
           ardillaFlag = 1;
           director.PlayCinematic(ardillaTime);
         }
+        // Interactuables
+        if (hitInfo.transform.gameObject.name == "InteractableSafe")
+        {
+          Destroy(bubbleUse);
+          bubble(kikiTransform, "La caja fuerte está intacta");
+          Destroy(hitInfo.transform.gameObject);
+        }
+        if (hitInfo.transform.gameObject.name == "InteractableChocolate")
+        {
+          Destroy(bubbleUse);
+          bubble(lidiaTransform, "Pueden comer de los bombones de chocolate \n como agradecimiento");
+          Destroy(hitInfo.transform.gameObject);
+        }
+        if (hitInfo.transform.gameObject.tag == "Interactable")
+        {
+          Destroy(bubbleUse);
+          randomMessagge();
+          //Destroy(hitInfo.transform.gameObject);
+        }
       }
+    }
+
+    if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+      Destroy(bubbleUse);
     }
   }
 
@@ -101,5 +112,49 @@ public class TestCollider : MonoBehaviour
     bubbleUse.transform.position = Camera.main.WorldToScreenPoint(charTransform.position + localPosition);
     bubbleUse.GetComponent<ChatBubble>().Setup(text);
     Destroy(bubbleUse.gameObject, 6f);
+  }
+
+  private void randomMessagge(){
+    Transform felixT = felixTransform;
+    string[] felixMensaje = new string[] {
+      "Esto no es interesante.",
+      "Concéntrate, esto no es una pista",
+      "Esto no será de mucha ayuda.",
+      "Mejor haz sonar trompeta si no vas a buscar buenas pistas",
+      "Deja ese objeto, no es de utilidad",
+      "Te equivocas, esto no es interesante"};
+    string randomFelix = felixMensaje[Random.Range(0, felixMensaje.Length)];
+    bubble(felixT, randomFelix);
+
+    Transform adelaT = adelaTransform;
+    string[] adelaMensaje = new string[] {
+      "Sé astuta, esto no será de ayuda","Dudo que esto sea importante",
+      "No te engañes a ti misma, esto no es interesante", 
+      "Concéntrate en tu habilidad,\n piensa como el malhechor",
+      "¡Es imposible que esto sea una pista!","¡Caramba, vaya desorden!"};
+    string randomAdela = adelaMensaje[Random.Range(0, adelaMensaje.Length)];
+    bubble(adelaT, randomAdela);
+
+    Transform rolloT = rolloTransform;
+    string[] rolloMensaje = new string[] {
+      "No es posible que esto sea una pista",
+      "No hay nada más aburrido para un detective \n que no encontrar pistas",
+      "¿Qué te hace pensar que será una pista interesante?", 
+      "Este tipo de objetos es lo que tenemos \n que evitar al investigar",
+      "Mejor será que nos concentremos",
+      "No hay ninguna huella aquí"};
+    string randomRollo = rolloMensaje[Random.Range(0, rolloMensaje.Length)];
+    bubble(rolloT, randomRollo);
+
+    Transform kikiT = kikiTransform;
+    string[] kikiMensaje = new string[] {
+      "Ardilla, ¿en serio crees que esto pueda ser de utilidad?", 
+      "concéntrate y ayúdame ardilla",
+      "Me estás distrayendo ardilla, esto no es interesante",
+      "La importancia de este objeto en el caso \n es tan pequeña como mi ardilla",
+      "Esto es realmente insignificante para nosotros", 
+      "Será mejor que nos concentremos"};
+    string randomKiki = kikiMensaje[Random.Range(0, kikiMensaje.Length)];
+    bubble(kikiT, randomKiki);
   }
 }
